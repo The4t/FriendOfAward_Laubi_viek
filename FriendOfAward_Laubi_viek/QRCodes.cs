624ceb6afd05ba58eaa4f9303e10faa104040557
Laube21 <sebi.laubichler@gmail.com>
2025-12-15 08:34:02 +0100

    generated QRCodes and upgrade QRControllers, Program.cs and generated AppDbContext

---
+++ FriendOfAward_Laubi_viek/QRCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendOfAward_Laubi_viek
+{
+    public class QRCode
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
