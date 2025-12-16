0578fbc64a9405b8192dee6ad34a2df36414d8f8
The4t <Kilian.viertbauer@gmail.com>
2025-12-16 11:25:51 +0100

    Alles neu versuch 1#

--- FriendOfAward_Laubi_viek/Program.cs
+++ FriendOfAward_Laubi_viek/Program.cs
@@ -4,14 +4,20 @@ using FriendOfAward_Laubi_viek.Components;
 using QRCoder;
 
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
+
+
+
 builder.Services.AddSingleton<AuthServiceSimple>();
 
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -24,6 +30,7 @@ app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages:
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
